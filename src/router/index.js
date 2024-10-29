60834f127d90e4e100f94d6cbbbf68a43db2d02b
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-10-29 11:05:40 +0900

    add previous and next button pagination

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
 import EventDetailsView from '../views/EventDetailsView.vue'
 import AboutView from '../views/AboutView.vue'
-import SimpleFormView from '@/views/SimpleFormView.vue';
+import SimpleFormView from '@/views/SimpleFormView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +11,7 @@ const router = createRouter({
       path: '/',
       name: 'event-list',
       component: EventListView,
+      props: (route) => ({ page: parseInt(route.query.page) || 1 }),
     },
     {
       path: '/event/:id',
